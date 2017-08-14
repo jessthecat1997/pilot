4d937f03a1c616e8abf709e038b9c462e7c6f364
Skipper <skippergcsnz@gmail.com>
2017-08-15 01:13:02 +0800

    payment updatev2

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -27,8 +27,21 @@ class PaymentsController extends Controller
 		->where('consignee_service_order_headers.id', '=', $id)
 		->get();
 
+		$rev = DB::table('billing_revenues')
+		->join('billings', 'billing_revenues.bill_id', '=', 'billings.id')
+		->select(DB::raw('CONCAT(SUM(amount)) as Total'))
+		->where('billing_revenues.bi_head_id','=', $id)
+		->get();
+
+		$exp = DB::table('billing_expenses')
+		->join('billings', 'billing_expenses.bill_id', '=', 'billings.id')
+		->select(DB::raw('CONCAT(SUM(amount)) as Total'))
+		->where('billing_expenses.bi_head_id','=', $id)
+		->get();
+
+
 		$so_head_id = $id;
-		return view('payment/payment_index', compact(['pays', 'so_head_id']));
+		return view('payment/payment_index', compact(['pays', 'so_head_id', 'exp', 'rev']));
 
 	}
 	public function payments_table(Request $request, $id)
@@ -57,15 +70,13 @@ class PaymentsController extends Controller
 	}
 	public function payment_pdf(Request $request, $id)
 	{
-		$payments = DB::table('payments')
-		->leftjoin('consignee_service_order_headers', 'payments.so_head_id', '=', 'consignee_service_order_headers.id')
-		->leftjoin('consignee_service_order_details', 'consignee_service_order_headers.id', '=', 'consignee_service_order_details.so_headers_id')
-		->leftjoin('consignees', 'consignee_service_order_headers.consignees_id', '=', 'consignees.id')
-		->leftjoin('payment_modes', 'payments.payment_mode_id', '=' , 'payment_modes.id')
-		->select('consignee_service_order_details.id','companyName','payments.amount', 'payment_modes.description', 'address', 'payments.created_at','TIN', 'businessStyle')
-		->where('payments.so_head_id','=',$id)
+		$payments = DB::table('consignee_service_order_headers')
+		->join('consignee_service_order_details', 'consignee_service_order_headers.id', '=', 'consignee_service_order_details.so_headers_id')
+		->join('consignees', 'consignee_service_order_headers.consignees_id','=','consignees.id')
+		->join('service_order_types', 'consignee_service_order_details.service_order_types_id', '=', 'service_order_types.id')
+		->select('consignee_service_order_headers.id', 'companyName', 'service_order_types.name', 'address')
+		->where('consignee_service_order_headers.id', '=', $id)
 		->get();
-
 		$pdf = PDF::loadView('pdf_layouts.payment_receipt', compact('payments'));
 		return $pdf->stream();
 	}
