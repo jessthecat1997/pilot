926398ca5407c5382d7068a2e6e81216b4b5f5bd
Unknown <yssareyes_15@yahoo.com>
2017-09-11 15:28:53 +0800

    migrations

---
+++ database/migrations/2017_09_11_064730_create_arrastre_dc_details.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArrastreDcDetails extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('arrastre_dc_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('arrastre_dc_headers_id')->unsigned();
+            $table->integer('dc_types_id')->unsigned();
+            $table->decimal('amount' , 10, 2);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('arrastre_dc_headers_id')->references('id')->on('arrastre_dc_headers');
+            $table->foreign('dc_types_id')->references('id')->on('dangerous_cargo_types');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('arrastre_dc_details');
+    }
+}
