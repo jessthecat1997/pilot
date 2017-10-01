4edd11c9ff40057a1aedb9baa4d0eb37ba74a6d0
Vane <yssareyes_15@yahoo.com>
2017-10-01 19:48:20 +0800

    general settings start

--- app/UtilityType.php
+++ app/UtilityType.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class UtilityType extends Model
 {
     protected $fillable = [
-	'contract_template', 'quotation_template', 'bank_charges', 'other_charges'
+	'contract_template', 'quotation_template', 'bank_charges', 'other_charges','insurance_gc','insurance_c','company_logo','company_name','company_address','company_tin','company_contact'
 	];
 
 	
