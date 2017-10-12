c7073dd94b94b2ee14a8099930362cc19c3de5eb
Unknown <yssareyes_15@yahoo.com>
2017-10-12 16:27:48 +0800

    order brokerage
    Remigio!!! paki kuha yung session ilagay sa consignee na select2

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -20,6 +20,16 @@ class OrdersController extends Controller
 
 		return view('order.order_index', compact(['orders','employees', 'consignees']));
 	}
+	public function setConsigneeID(){
+
+		$so_head = \DB::table('consignee_service_order_headers')
+		->select('*')
+		->join('consignees as A', 'consignee_service_order_headers.consignees_id', '=', 'A.id')
+		->where('consignee_service_order_headers.id','=',$id)
+		->get();
+
+		session(['consignees_id' => '{{$so_head[0]->consignees_id}}' ]);
+	}
 
 	public function show($id){
 		
