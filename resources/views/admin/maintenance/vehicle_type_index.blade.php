572102740fc958d3f6482f8161d923edf08a1955
Unknown <issej27@yahoo.com.ph>
2017-10-16 16:01:09 +0800

    remove img

--- resources/views/admin/maintenance/vehicle_type_index.blade.php
+++ resources/views/admin/maintenance/vehicle_type_index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class = "container-fluid">
 	<div class = "row">
-		<h3><img src="/images/bar.png"> Maintenance | Delivery | Vehicle Type</h3>
+		<h3>Maintenance | Delivery | Vehicle Type</h3>
 		<hr>
 		<div class = "col-md-3 col-md-offset-9">
 			<button  class="btn btn-info btn-md new" data-toggle="modal" data-target="#vtModal" style = "width: 100%;">New Vehicle Type</button>
